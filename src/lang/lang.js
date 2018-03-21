f5ad7cd90e3e1f29ab57f4b7b188917f3f31db63
Linus Willner <lwtechgaming@gmail.com>
2018-03-21 22:59:53 +0200

    Implement backwards compatibility
    * Fixed a crash when the lang property in the local storage would be different due to the previous version of the website

--- src/lang/lang.js
+++ src/lang/lang.js
@@ -17,9 +17,18 @@ export function i18n (stringPath) {
   // Convert array from [ 'obj.prop' ] to [ 'obj', 'prop' ]
   stringPath = stringPath[0].split('.')
 
-  // Ramda handles the pathfinding in the object
-  let res = R.path(stringPath, window.lang[localStorage.getItem('lang') || 'gb'])
+  // Check that the lang value is correct
+  if (localStorage.getItem('lang') && localStorage.getItem('lang').match(/gb|fi|se/gi) !== null) {
+    // Ramda handles the pathfinding in the object
+    let res = R.path(stringPath, window.lang[localStorage.getItem('lang') || 'gb'])
 
-  if (res === undefined) return ''
-  else return res
+    if (res === undefined) return ''
+    else return res
+  } else {
+    // Backwards compatibility to previous version of the website
+    // Old: en/fi/sv
+    // New: gb/fi/se
+    localStorage.setItem('lang', 'gb')
+    window.location.reload(true)
+  }
 }
