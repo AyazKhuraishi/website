5710dfeee8235b5ca638a3c437a91906236f2b63
Linus Willner <hello@linuswillner.me>
2020-05-13 17:48:31 +0300

    Rework project cards and button layouts

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,33 +1,37 @@
 // Project card
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { ButtonGroup, ProjectButton } from './Buttons'
+import { ProjectButton } from './ProjectButton'
 
 export default class ProjectCard extends Component {
   static propTypes = {
     title: PropTypes.string.isRequired,
     text: PropTypes.string.isRequired,
-    links: PropTypes.object
+    links: PropTypes.exact({
+      github: PropTypes.string,
+      website: PropTypes.string
+    })
   }
 
-  getContent () {
-    const { website, github } = this.props.links
+  generateLinks = () => {
+    const { links } = this.props
+    const buttons = []
 
-    if (website && github) {
-      return <ButtonGroup github={github} website={website}/>
-    } else if (website) {
-      return <ProjectButton type='website' link={website}/>
-    } else if (github) {
-      return <ProjectButton type='github' link={github}/>
-    }
+    if (links.github) buttons.push(<ProjectButton key='gh' type='github' link={links.github}/>)
+    if (links.npm) buttons.push(<ProjectButton key='npm' type='npm' link={links.npm}/>)
+    if (links.website) buttons.push(<ProjectButton key='web' type='website' link={links.website}/>)
+
+    return buttons
   }
 
   render () {
     return (
       <div className='tile card notification is-child'>
-        <h2 className='section-title card-title'>{this.props.title}</h2>
+        <h2 className='section-title card-title'>
+          {this.props.title}
+        </h2>
+        <p className='project-links'>{this.generateLinks()}</p>
         <p className='section-text card-text'>{this.props.text}</p>
-        {this.getContent()}
       </div>
     )
   }
