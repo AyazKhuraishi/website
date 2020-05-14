6da020572d772efb05e6893c84516dbd522bc4e1
Linus Willner <hello@linuswillner.me>
2020-05-15 00:44:59 +0300

    Fix content pointer bounding bugs and prevent subtitle from disappearing

--- src/components/ContentPointer.jsx
+++ src/components/ContentPointer.jsx
@@ -1,14 +1,62 @@
 import React, { Component } from 'react'
 
 export default class ContentPointer extends Component {
+  constructor (props) {
+    super(props)
+    this.state = { isOverlapping: false }
+  }
+
+  checkDescriptionOverlap = () => {
+    // I could *theoretically* use refs for this, but that would involve uplifting refs so much that it would just be wasted effort
+    const contentPointerRect = document.getElementById('content-pointer')?.getBoundingClientRect()
+    const subtitleRect = document.getElementById('subtitle')?.getBoundingClientRect()
+
+    // First check if element is above the description, where it should not be - if so, short-circuit
+    const isAbove = ((contentPointerRect.top < subtitleRect.top) && (contentPointerRect.bottom < subtitleRect.bottom))
+
+    if (isAbove) return true
+    else {
+      // Check if any property exists since Object.keys() doesn't apparently works on bounding rects
+      if (contentPointerRect?.x && subtitleRect?.x) {
+        const isOverlapping = !(
+          contentPointerRect.right < subtitleRect.left ||
+          contentPointerRect.left > subtitleRect.right ||
+          contentPointerRect.bottom < subtitleRect.top ||
+          contentPointerRect.top > subtitleRect.bottom
+        )
+
+        return isOverlapping
+      }
+    }
+  }
+
+  checkOverlapState = () => {
+    const isOverlapping = this.checkDescriptionOverlap()
+    if (isOverlapping !== this.state.isOverlapping) this.setState({ isOverlapping })
+  }
+
   handleClick = () => {
     document.getElementById('about').scrollIntoView({ behavior: 'smooth' })
   }
 
+  componentDidMount () {
+    // This looks quite stupid, but it works with not letting the arrow appear on page load
+    setTimeout(this.checkOverlapState, 0)
+    window.addEventListener('resize', () => this.checkOverlapState())
+  }
+
   render () {
+    const styles = {
+      hidden: {
+        opacity: 0
+      }
+    }
+
     return (
       <div
+        id='content-pointer'
         className='content-pointer'
+        style={this.state.isOverlapping ? styles.hidden : {}}
         onClick={this.handleClick}
       >
         <i className='cil-chevron-double-down'/>
--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -15,10 +15,13 @@ export default class Hero extends Component {
 
   componentDidMount () {
     dispatcher.on('WIDTH_CHANGE', isLowWidth => this.setState({ typingEnabled: !isLowWidth }))
+
     dispatcher.on('LANG_SELECT', () => {
-      this.setState({ typingEnabled: false }, () => {
-        this.setState({ typingEnabled: true })
-      })
+      if (!window.lowWidth) {
+        this.setState({ typingEnabled: false }, () => {
+          this.setState({ typingEnabled: true })
+        })
+      }
     })
   }
 
@@ -34,11 +37,13 @@ export default class Hero extends Component {
         <div className='hero-body'>
           <div className='container has-text-centered'>
             <h1 className='large-title' style={this.state.typingEnabled ? {} : styles.titleOverrides}>Linus Willner</h1>
-            {
-              this.state.typingEnabled
-                ? <Console/>
-                : <div className='subtitle-lowwidth'><h2>{i18n`hero.typingTextLowWidth`}</h2></div>
-            }
+            <div id='subtitle'>
+              {
+                this.state.typingEnabled
+                  ? <Console/>
+                  : <div className='subtitle-lowwidth'><h2>{i18n`hero.typingTextLowWidth`}</h2></div>
+              }
+            </div>
           </div>
         </div>
       </div>
